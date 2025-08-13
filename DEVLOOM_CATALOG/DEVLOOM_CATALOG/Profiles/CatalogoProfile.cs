dd19d6aebb6f251d83032e73f4dadba8672c7883
Dávisson Falcão <davissonbeat@hotmail.com>
2025-08-13 09:48:43 -0300

    feat: criar profiles para mapeamento do automapper

---
+++ DEVLOOM_CATALOG/DEVLOOM_CATALOG/Profiles/CatalogoProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DEVLOOM_CATALOG.Domain;
+using DEVLOOM_CATALOG.Application.DTOs;
+
+namespace DEVLOOM_CATALOG.Application.Mappings
+{
+    public class CatalogoProfile : Profile
+    {
+        public CatalogoProfile()
+        {
+            // Categoria
+            CreateMap<CategoriaRequestDto, Categoria>();
+            CreateMap<Categoria, CategoriaResponseDto>();
+
+            // Produto
+            CreateMap<ProdutoRequestDto, Produto>();
+            CreateMap<Produto, ProdutoResponseDto>();
+        }
+    }
+}
