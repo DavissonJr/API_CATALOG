ba0fe10183f0a18dd4963fae444b5cd8abfd276e
Dávisson Falcão <davissonbeat@hotmail.com>
2025-08-13 09:36:53 -0300

    chore: instalar itens pacotes para garantir funcionamento da API

--- DEVLOOM_CATALOG/DEVLOOM_CATALOG/Program.cs
+++ DEVLOOM_CATALOG/DEVLOOM_CATALOG/Program.cs
@@ -1,25 +1,59 @@
+using DEVLOOM_CATALOG.Domain;
+using DEVLOOM_CATALOG.Infra.Interfaces;
+using DEVLOOM_CATALOG.Infra.Repositories;
+using DEVLOOM_CATALOG.Services.Interfaces;
+using DEVLOOM_CATALOG.Services.Service;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+// Registrar AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Registrar repositories
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+// Registrar services
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
+
+// Registrar IDbConnection (exemplo usando SQL Server)
+builder.Services.AddScoped<System.Data.IDbConnection>(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Connection");
+    return new SqlConnection(connectionString);
+});
+
+// Registrar Swagger/OpenAPI (opcional, mas útil)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
