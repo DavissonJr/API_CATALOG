5d96552a96b0d446d53ab36e6ea66df7c81b20ea
Dávisson Falcão <davissonbeat@hotmail.com>
2025-08-13 09:06:35 -0300

    feat: criar as entidades

---
+++ DEVLOOM_CATALOG/DEVLOOM_CATALOG/Domain/Categoria.cs
@@ -0,0 +1,15 @@
+﻿namespace DEVLOOM_CATALOG.Domain
+{
+    public class Categoria
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+        public bool Ativo { get; set; }
+
+        // Relacionamento 1:N
+        public List<Produto> Produtos { get; set; } = new();
+    }
+}
+
---
+++ DEVLOOM_CATALOG/DEVLOOM_CATALOG/Domain/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace DEVLOOM_CATALOG.Domain
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal PrecoUnitario { get; set; }
+        public Guid CategoriaId { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+        public bool Ativo { get; set; }
+
+        // Relacionamento N:1
+        public Categoria? Categoria { get; set; }
+    }
+}
